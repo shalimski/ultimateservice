61e61b6a95752aeaab8f7798d50065d469acf93c
shalimski <shalimski@gmail.com>
2022-08-14 13:53:38 +0800

    add logger

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 
+	"github.com/shalimski/ultimateservice/pkg/logger"
 	_ "go.uber.org/automaxprocs"
 )
 
 var build = "develop"
 
 func main() {
-	log.Printf("starting sales-api build[%s] CPU[%d]\n", build, runtime.NumCPU())
+	log, err := logger.New("SALES-API")
+	if err != nil {
+	}
+
+	defer log.Sync()
+
+	log.Infof("starting sales-api build[%s] CPU[%d]\n", build, runtime.NumCPU())
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	log.Println("stopping sales-api")
+	log.Info("stopping sales-api")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/shalimski/ultimateservice
 
 go 1.17
 
-require go.uber.org/automaxprocs v1.5.1
+require (
+	go.uber.org/automaxprocs v1.5.1
+	go.uber.org/zap v1.22.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
