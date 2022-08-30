b53f6030d78e94dc10df88b583b93fbbac207fc5
shalimski <shalimski@gmail.com>
2022-08-30 21:42:33 +0800

    add db package

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/google/uuid v1.3.0
 	github.com/ilyakaznacheev/cleanenv v1.3.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.6
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/automaxprocs v1.5.1
 	go.uber.org/zap v1.22.0
--- internal/config/config.go
+++ internal/config/config.go
@@ -15,6 +15,19 @@ type Config struct {
 	ShutdownTimeout time.Duration `env:"SHUTDOWNTIMEOUT" env-default:"20s"`
 	AuthKeysFolder  string        `env:"AUTH_KEYFOLDER" env-default:"infra/keys/"`
 	AuthActiveKID   string        `env:"AUTH_ACTIVEKID" env-default:"c2e055bb-f637-4cc3-9b4a-916a8b31304a"`
+
+	// Database PostgreSQL
+	DB DB
+}
+
+type DB struct {
+	User         string `env:"DB_USER" env-default:"postgres"`
+	Password     string `env:"DB_PASSWORD" env-default:"postgres"`
+	Host         string `env:"DB_HOST" env-default:"localhost"`
+	Name         string `env:"DB_NAME" env-default:"postgres"`
+	MaxIdleConns int    `env:"DB_MAXIDLE" env-default:"0"`
+	MaxOpenConns int    `env:"DB_MAXOPEN" env-default:"0"`
+	DisableTLS   bool   `env:"DB_DISABLETLS" env-default:"true"`
 }
 
 type auth struct{}
