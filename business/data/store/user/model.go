e979ea2649b5dd36d4546eec3972f29a40fd3275
shalimski <shalimski@gmail.com>
2022-09-04 21:43:40 +0800

    prepare store for user

---
+++ business/data/store/user/model.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type User struct {
+	ID           string         `db:"user_id"`
+	Name         string         `db:"name"`
+	Email        string         `db:"email"`
+	Roles        pq.StringArray `db:"roles"`
+	PasswordHash []byte         `db:"password_hash"`
+	DateCreated  time.Time      `db:"date_created"`
+	DateUpdated  time.Time      `db:"date_updated"`
+}
